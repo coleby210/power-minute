9bd194b1806047e9aee0727d75dec48d0c0c61ed
Coleby Kent & Ryan Ho <pair+coleby210.ryanjho@devbootcamp.com>
2016-02-19 18:03:17 -0800

    Add back buttons to category/show, workout_template/show pages. Add settings in navbar

--- app/controllers/workout_templates_controller.rb
+++ app/controllers/workout_templates_controller.rb
@@ -2,6 +2,7 @@ class WorkoutTemplatesController < ApplicationController
   before_action :authenticate_user!
 
   def show
+    @category = params[:category_id]
     @workout_template = WorkoutTemplate.find(params[:id])
   end
 end
