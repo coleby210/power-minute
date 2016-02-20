a040d44e90aa372ad2032654cf09fe0f7e58093d
Coleby Kent <coleby210@gmail.com>
2016-02-20 13:37:03 -0800

    Cleaned up code

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,7 +1,12 @@
 class WorkoutsController < ApplicationController
   def create
-    Workout.create(user_id: current_user.id, workout_template_id: params[:workouts][:workout_template_id])
+    current_user.workouts.new(workout_params).save
 
     redirect_to profile_path(current_user)
+  end
+
+  private
+  def workout_params
+    params.require(:workouts).permit(:workout_template_id)
   end
 end
