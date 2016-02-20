929d657b7f1f6624fdc36f8e388752fa0824cc6e
Beni Shpringer <email": null>
2016-02-19 16:52:33 -0800

    Refactor users controller, move methods to model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,51 +1,13 @@
 class UsersController < ApplicationController
-
+  # before_action :verify_user
   def show
-    @user = current_user
+    @user = User.find(params[:id])
     @total_workouts = @user.workouts
     @distinct_workouts = @total_workouts.select(:workout_template_id).distinct
-    sort_most_common_workouts
-    sort_most_common_categories
-    # @workout_names = find_workout_names(@workouts)
-
-    render :show
   end
-
-  def sort_most_common_workouts
-    @completed_workouts = {}
-
-    @distinct_workouts.map do |workout|
-      @completed_workouts["#{workout.find_workout_template_name}"] =  workout.calculate_workout_instances(@total_workouts)
-    end
-
-    @completed_workouts = Hash[@completed_workouts.sort_by{|k,v| v}.reverse]
-
-  end
-
-  def sort_most_common_categories
-    @completed_categories = {}
-
-    Category.all.each do |category|
-      @completed_categories["#{category.name}"] = category.calculate_category_instances(@user)
-    end
-
-    @completed_categories = Hash[@completed_categories.sort_by{|k,v| v}.reverse]
-  end
-
 
   protected
 
-  # # Find the names of all workouts performed by a user
-  # def find_workout_template_name(workout)
-  #   WorkoutTemplate.find(workout.workout_template_id).title
 
-  #   # workout_names = []
-
-  #   # workouts.each do |workout|
-  #   #   workout_names << WorkoutTemplate.find(workout.workout_template_id).title
-  #   # end
-
-  #   # workout_names
-  # end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,28 @@ class User < ActiveRecord::Base
   has_many :workout_templates, foreign_key: :creator_id
   has_many :workouts
 
+  def sort_most_common_workouts
+    @total_workouts = self.workouts
+    @distinct_workouts = @total_workouts.select(:workout_template_id).distinct
+
+    @completed_workouts = {}
+
+    @distinct_workouts.map do |workout|
+      @completed_workouts["#{workout.find_workout_template_name}"] =  workout.calculate_workout_instances(@total_workouts)
+    end
+
+    Hash[@completed_workouts.sort_by{|k,v| v}.reverse]
+  end
+
+  def sort_most_common_categories
+    @total_workouts = self.workouts
+    @completed_categories = {}
+
+    Category.all.each do |category|
+      @completed_categories["#{category.name}"] = category.calculate_category_instances(self)
+    end
+    Hash[@completed_categories.sort_by{|k,v| v}.reverse]
+  end
+
+
 end
