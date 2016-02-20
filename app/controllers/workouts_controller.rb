78dd842e6577cccfd84648ee30905ad65665d0c8
Coleby Kent <coleby210@gmail.com>
2016-02-20 13:25:00 -0800

    Complete the timer and adding of workouts

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,7 @@
 class WorkoutsController < ApplicationController
+  def create
+    Workout.create(user_id: current_user.id, workout_template_id: params[:workouts][:workout_template_id])
+
+    redirect_to profile_path(current_user)
+  end
 end
