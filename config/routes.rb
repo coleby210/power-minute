543bd658364efe92cb9d195f8797db29fe56d7fd
Coleby Kent <coleby210@gmail.com>
2016-02-20 18:07:15 -0800

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,13 @@ Rails.application.routes.draw do
       }
 
   get "users/:id", to: "users#show", as: "profile"
-  # get 'auth/:provider/callback', to: 'sessions#create'
-  # get 'auth/failure', to: redirect('/')
-  # get 'signout', to: 'sessions#destroy', as: 'signout'
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
 
   resources :categories
   resources :workout_templates
   resources :workouts
-  # resources :sessions, only: [:create, :destroy]
-  # resource :home, only: [:show]
 
   root "categories#index"
 
