0e9d9ade88dd47df5c3d769947d30b02fb29c905
Beni Shpringer <email": null>
2016-02-21 17:48:32 -0800

    fix other peoples bugs

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -40,12 +40,12 @@ before_filter :configure_account_update_params, only: [:update]
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.for(:sign_up) << :first_name << :last_name
+    devise_parameter_sanitizer.for(:sign_up) << :name
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.for(:account_update) << :first_name << :last_name
+    devise_parameter_sanitizer.for(:account_update) << :name
   end
 
   # The path used after sign up.
