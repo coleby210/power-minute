534ef4fea37141da877b9d48db434791ba7ec6ff
Coleby Kent <coleby210@gmail.com>
2016-02-22 15:33:26 -0800

    Finish adding first/last name for facebook and registraion

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222004218) do
+ActiveRecord::Schema.define(version: 20160222225627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,7 +69,9 @@ ActiveRecord::Schema.define(version: 20160222004218) do
     t.integer  "phone_number"
     t.string   "provider"
     t.string   "uid"
-    t.string   "name"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "timezone"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
