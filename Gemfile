5c1a287fce880150ce55f1c77276e83a96c2eebb
Sean Massih <email": null>
2016-02-23 10:29:16 -0800

    Create new view file

--- Gemfile
+++ Gemfile
@@ -29,6 +29,9 @@ gem 'faker'
 gem 'chartjs-ror'
 gem 'omniauth-facebook'
 
+# Incorporate calendar functionality
+gem "simple_calendar", "~> 2.0"
+
 gem 'rails_12factor', group: :production
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :set_time_zone, if: :user_signed_in?
+
   def index
+  end
+
+  private
+
+  def set_time_zone
+    Time.zone = current_user.time_zone
   end
 
 end
