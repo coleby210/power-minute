9d48d4e8a7c51cd3eb8d4d037133d00468cd4557
Beni Shpringer <email": null>
2016-02-25 17:14:14 -0800

    Fix logging workouts

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -3,10 +3,9 @@ class WorkoutsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-
-    tagged_ids = params[:tagged_ids].values
     current_user.workouts.new(workout_params).save
-    unless tagged_ids.length == 0
+    if params[:tagged_ids]
+      tagged_ids = params[:tagged_ids].values
       tagged_ids.each do |user_id|
         Workout.create(user_id: user_id, workout_template_id: params[:workouts][:workout_template_id])
       end
